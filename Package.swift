73eb8294686555a7dd0f23cddd3db9cac0aa58b0
Paz Lavi <paz.lavi@appsflyer.com>
2021-09-26 17:18:47 +0300

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "AppsFlyerResolver",
+    name: "AppsFlyerURLResolver",
     platforms: [
            .iOS(.v9)
        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AppsFlyerResolver",
-            targets: ["AppsFlyerResolver"]),
+            name: "AppsFlyerURLResolver",
+            targets: ["AppsFlyerURLResolver"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,12 +22,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "AppsFlyerResolver",
+            name: "AppsFlyerURLResolver",
             dependencies: [],
-            path: "iOS/AppsFlyerURLResolver/Sources/AppsFlyerResolver"),
+            path: "iOS/AppsFlyerURLResolver/Sources/AppsFlyerURLResolver"),
         .testTarget(
-            name: "AppsFlyerResolverTests",
-            dependencies: ["AppsFlyerResolver"],
+            name: "AppsFlyerURLResolverTests",
+            dependencies: ["AppsFlyerURLResolver"],
             path: "Tests"),
     ]
 )
