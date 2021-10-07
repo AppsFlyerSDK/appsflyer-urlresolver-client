31d452fb9de3eb92b8ec5053cbaae0d6d9efd775
Paz Lavi <paz.lavi@appsflyer.com>
2021-10-07 13:13:20 +0300

    Use Callbacks

--- Android/AppsFlyerURLResolver/AppsFlyerURLResolverLib/src/androidTest/java/com/appsflyer/resolver/UrlResolverJavaTest.java
+++ Android/AppsFlyerURLResolver/AppsFlyerURLResolverLib/src/androidTest/java/com/appsflyer/resolver/UrlResolverJavaTest.java
@@ -2,20 +2,41 @@ package com.appsflyer.resolver;
 
 import static org.junit.Assert.assertEquals;
 
+import androidx.annotation.Nullable;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 public class UrlResolverJavaTest {
+    private final CountDownLatch lock = new CountDownLatch(1);
+    String res = null;
+
+    @Before
+    public void init() {
+        res = null;
+    }
+
     @Test
-    public void testResolveUsingBitlyAndLambda() {
-        String res = new URLResolver(true)
-                .resolveSync("https://bit.ly/38JtcFq", 5);
+    public void testResolveUsingBitlyAndLambda() throws InterruptedException {
+        new URLResolver(true)
+                .resolve("https://bit.ly/38JtcFq", 5, url -> res = url);
+        lock.await(5, TimeUnit.SECONDS);
         assertEquals(res, "https://paz.onelink.me/waF3/paz");
     }
 
     @Test
-    public void testResolveUsingBitlyAndCallback() {
-        String res = new URLResolver()
-                .resolveSync("https://bit.ly/38JtcFq", 5);
+    public void testResolveUsingBitlyAndCallback() throws InterruptedException {
+        new URLResolver()
+                .resolve("https://bit.ly/38JtcFq", 5, new URLResolverListener() {
+                    @Override
+                    public void onComplete(@Nullable String url) {
+                        res = url;
+                    }
+                });
+        lock.await(5, TimeUnit.SECONDS);
         assertEquals(res, "https://paz.onelink.me/waF3/paz");
     }
 }
--- Android/AppsFlyerURLResolver/AppsFlyerURLResolverLib/src/androidTest/java/com/appsflyer/resolver/UrlResolverTest.kt
+++ Android/AppsFlyerURLResolver/AppsFlyerURLResolverLib/src/androidTest/java/com/appsflyer/resolver/UrlResolverTest.kt
@@ -1,25 +1,37 @@
 package com.appsflyer.resolver
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 class UrlResolverTest {
+    private val lock: CountDownLatch = CountDownLatch(1)
+
     @Test
     fun testResolveUsingBitlyAndLambda() {
-        val res = URLResolver(true).resolveSync("https://bit.ly/38JtcFq", 5)
+        var res: String? = null
+        URLResolver(true).resolve("https://bit.ly/38JtcFq", 5) {
+            res = it
+        }
+        lock.await(5, TimeUnit.SECONDS)
         assertEquals(res, "https://paz.onelink.me/waF3/paz")
     }
 
     @Test
     fun testResolveMILink() {
-        val res = URLResolver().resolveSync(
+        var res: String? = null
+        URLResolver(true).resolve(
             "https://mi.gap.com/p/cp/d46c31d50872e4b3/c?mi_u=51635872&EV=GPUSCPATHTSTPC40485" +
                     "93PERST1_WMNNLPSPRP04162021&DI=51635872&CD=GPNC_GPR&cvosrc=email.exacttarget" +
                     ".GPUS04162021&EV_Segment1=GPUSGWMET&url=https%3A%2F%2Fmi.gap.com%2Fp%2Frp%2F" +
                     "6d2aa0caedfe8322%2Furl&mi_u=%%email_key%%&EV=%%EV_value%%&DI=%%DI_value%%&CD" +
                     "=%%CD_value%%",
-            5
-        )
+        ) {
+            res = it
+        }
+        lock.await(5, TimeUnit.SECONDS)
         assertEquals(
             res,
             "https://www.gap.com/?mi_u=%25%25email_key%25%25&EV=%25%25EV_value%25%25&DI=%2" +
@@ -29,38 +41,89 @@ class UrlResolverTest {
     }
 
     @Test
+    fun testReachToMaximum() {
+        var res: String? = null
+        URLResolver(true).resolve(
+            "https://mi.gap.com/p/cp/d46c31d50872e4b3/c?mi_u=51635872&EV=GPUSCPATHTSTPC40485" +
+                    "93PERST1_WMNNLPSPRP04162021&DI=51635872&CD=GPNC_GPR&cvosrc=email.exacttarget" +
+                    ".GPUS04162021&EV_Segment1=GPUSGWMET&url=https%3A%2F%2Fmi.gap.com%2Fp%2Frp%2F" +
+                    "6d2aa0caedfe8322%2Furl&mi_u=%%email_key%%&EV=%%EV_value%%&DI=%%DI_value%%&CD" +
+                    "=%%CD_value%%",
+            2
+        ) {
+            res = it
+        }
+        lock.await(5, TimeUnit.SECONDS)
+        assertNotEquals(
+            res,
+            "https://www.gap.com/?mi_u=%25%25email_key%25%25&EV=%25%25EV_value%25%25&DI=%2" +
+                    "5%25DI_value%25%25&CD=%25%25CD_value%25%25&cvosrc=email.exacttarget.GPUS0416" +
+                    "2021"
+        )
+    }
+
+    @Test
     fun testResolveUsingBitlyAndCallback() {
-        val res = URLResolver().resolveSync("https://bit.ly/38JtcFq", 5)
+        var res: String? = null
+        URLResolver(true).resolve("https://bit.ly/38JtcFq", 5, object : URLResolverListener {
+            override fun onComplete(url: String?) {
+                res = url
+            }
+        })
+        lock.await(5, TimeUnit.SECONDS)
         assertEquals(res, "https://paz.onelink.me/waF3/paz")
+
     }
 
     @Test
     fun testResolveUsingBitlyAndCallbackObject() {
-        val res = URLResolver().resolveSync("https://bit.ly/38JtcFq", 5)
+        var res: String? = null
+        val listener = object : URLResolverListener {
+            override fun onComplete(url: String?) {
+                res = url
+            }
+        }
+        URLResolver(true).resolve("https://bit.ly/38JtcFq", 5, listener)
+        lock.await(5, TimeUnit.SECONDS)
         assertEquals(res, "https://paz.onelink.me/waF3/paz")
+
     }
 
     @Test
     fun testNullURL() {
-        val res = URLResolver().resolveSync(null, 5)
+        var res: String? = null
+        URLResolver(true).resolve(null, 5) {
+            res = it
+        }
+        lock.await(5, TimeUnit.SECONDS)
         assertEquals(res, null)
+
     }
 
     @Test
     fun testNotaURL() {
-        val res = URLResolver().resolveSync("abcd", 5)
+        var res: String? = null
+        URLResolver(true).resolve("abcd", 5) {
+            res = it
+        }
+        lock.await(5, TimeUnit.SECONDS)
         assertEquals(res, "abcd")
     }
 
     @Test
     fun testEmptyString() {
-        val res = URLResolver().resolveSync("", 5)
+        var res: String? = null
+        URLResolver(true).resolve("", 5) {
+            res = it
+        }
+        lock.await(5, TimeUnit.SECONDS)
         assertEquals(res, "")
     }
 
     @Test
     fun testWhenURLisEncoded() {
-        val res = URLResolver().resolveSync(
+        var res: String? = "12"
+        URLResolver(true).resolve(
             "https%3A%2F%2Fmi.gap.com%2Fp%2Fcp%2Fd46c31d50872e4b3%2Fc%3Fmi_u%3D51635872%26EV" +
                     "%3DGPUSCPATHTSTPC4048593PERST1_WMNNLPSPRP04162021%26DI%3D51635872%26CD%3DGPN" +
                     "C_GPR%26cvosrc%3Demail.exacttarget.GPUS04162021%26EV_Segment1%3DGPUSGWMET%26" +
@@ -68,7 +131,10 @@ class UrlResolverTest {
                     "rl%26mi_u%3D%25%25email_key%25%25%26EV%3D%25%25EV_value%25%25%26DI%3D%25%25D" +
                     "I_value%25%25%26CD%3D%25%25CD_value%25%25",
             5
-        )
+        ) {
+            res = it
+        }
+        lock.await(5, TimeUnit.SECONDS)
         assertEquals(
             res,
             "https%3A%2F%2Fmi.gap.com%2Fp%2Fcp%2Fd46c31d50872e4b3%2Fc%3Fmi_u%3D51635872%26" +
