c8b767b3d04156e4e0ea8bbf1b027ce7707d9223
ivan.obodovskyi <ivan.obodovskyi@appsflyer.com>
2023-11-15 14:16:58 +0200

    Move all testing data to html files Add emersys JS resolving flow

--- iOS/AppsFlyerURLResolver/Sources/AppsFlyerURLResolver/JSRedirectionHandler.swift
+++ iOS/AppsFlyerURLResolver/Sources/AppsFlyerURLResolver/JSRedirectionHandler.swift
@@ -7,7 +7,15 @@
 
 import Foundation
 
-class JSRedirectionHandler : NSObject {
+final class JSRedirectionHandler : NSObject {
+    
+    private enum RegExp {
+        static let hubspotRegepx: String = "window.location.replace\\(\\\".*\"\\)"
+        static let emersysRegexp: String = "http-equiv=\"refresh\"[\\s\\n]*content=\"0;[\\s\\n]*URL='([^']+)'\""
+    }
+
+    
+    
     var jsRedirectionCompletionHandler : ((String?) -> Void)
     var logger : AppsFlyerLogger
     
@@ -50,21 +58,37 @@ class JSRedirectionHandler : NSObject {
      gets the link that the JS code returns for iOS platform
      */
     func extractLinkFrom(JSCode stringifyJSCode: String) -> String? {
-        let rawStringLinks = matches(for: "window.location.replace\\(\\\".*\"\\)", in: stringifyJSCode)
+        var rawStringLinks = matches(for: RegExp.hubspotRegepx, in: stringifyJSCode)
+        
+        if rawStringLinks.isEmpty {
+            rawStringLinks = matches(for: RegExp.emersysRegexp, in: stringifyJSCode)
+        }
+        
         if rawStringLinks.isEmpty {
             return nil
         }
-        let lastLink = rawStringLinks[rawStringLinks.count-1]
-        var redirectLink = matches(for: "http.*\"", in: lastLink).joined()
-        redirectLink = String(redirectLink.dropLast())
         
+        guard let lastLink = rawStringLinks.last else { return nil }
+        var redirectLink = detectURLInString(text: lastLink).joined()
         return redirectLink
+    }
+    
+    func detectURLInString(text: String) -> [String] {
+        let types: NSTextCheckingResult.CheckingType = [ .link]
+        let detector = try? NSDataDetector(types: types.rawValue)
+        guard let results = detector?.matches(in: text, range: NSRange(location: 0, length: text.count)) else { return [String]() }
+        
+        return results.map { String(text[Range($0.range, in: text)!]) }
     }
     
     /**
      finds occurences of sub-string inside string using regex. Returns array of sub-strings that match the regex or an empty array when something wrong happened
      */
-    func matches(for regex: String, in text: String) -> [String]{
+    func matches(for regex: String, in text: String) -> [String] {
+        return _matches(for: regex, in: text)
+    }
+    
+    private func _matches(for regex: String, in text: String) -> [String] {
         do {
             let regex = try NSRegularExpression(pattern: regex)
             let results = regex.matches(in: text, range: NSRange(text.startIndex..., in: text))
